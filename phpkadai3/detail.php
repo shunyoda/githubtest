625839e8fd94fad6eca04a56b8dcdf2d18dd1d76
shunyoda <80559999+shunyoda@users.noreply.github.com>
2021-04-08 15:11:51 +0900

    ブックマークアプリ更新・削除(DB管理)

---
+++ phpkadai3/detail.php
@@ -0,0 +1,58 @@
+<?php
+require_once('funcs.php');
+$pdo = db_conn();
+$id = $_GET['id'];
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare('SELECT * FROM gs_bm_table WHERE id=' . $id . ';');
+$status = $stmt->execute();
+
+//３．データ表示
+if ($status == false) {
+    sql_error($status);
+} else {
+    $result = $stmt->fetch();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>データ登録</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: bisque;
+        }
+        div {
+            padding: 10px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+            </div>
+        </nav>
+    </header>
+    <!-- method, action, 各inputのnameを確認してください。  -->
+    <form method="POST" action="update.php">
+        <div class="jumbotron">
+            <fieldset>
+                <legend>フリーアンケート</legend>
+                <label>名前：<input type="text" name="title" value="<?= $result['title'] ?>"></label><br>
+                <label>URL：<input type="text" name="url" value="<?= $result['url'] ?>"></label><br>
+                <label>コメント：<textarea name="comment" rows="4" cols="40"><?= $result['comment'] ?></textarea></label><br>
+                <!-- ↓追加 -->
+                <input type="hidden" name="id" value="<?= $result['id'] ?>">
+                <input type="submit" value="送信">
+            </fieldset>
+        </div>
+    </form>
+</body>
+</html>
---
+++ phpkadai3/insert.php
@@ -0,0 +1,27 @@
+<?php
+//1. POSTデータ取得
+$title   = $_POST["title"];
+$url  = $_POST["url"];
+$comment    = $_POST["comment"];
+
+//2. DB接続します
+//*** function化する！  *****************
+require_once('funcs.php');
+$pdo = db_conn();
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table(title,url,comment,indate)VALUES(:title,:url,:comment,sysdate())");
+
+// 数値の場合 PDO::PARAM_INT
+// 文字の場合 PDO::PARAM_STR
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+$status = $stmt->execute(); //実行
+
+//４．データ登録処理後
+if ($status == false) {
+    sql_error($stmt);
+} else {
+    redirect('index.php');
+}
