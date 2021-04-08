22329cd3e9cdd363b349c451f95176a39e4c7bf2
shunyoda <80559999+shunyoda@users.noreply.github.com>
2021-04-08 15:20:27 +0900

    ブックマークアプリのUSER管理画面_shunyoda_51

---
+++ ブックマークアプリのUSER管理画面_shunyoda_51/delete.php
@@ -0,0 +1,31 @@
+<?php
+//1. POSTデータ取得
+$id   = $_GET['id'];
+
+//2. DB接続します
+require_once('funcs.php');
+$pdo = db_conn();
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare(
+    'DELETE FROM
+        gs_bm_table
+    WHERE
+        id = :id
+    '
+);
+
+// 数値の場合 PDO::PARAM_INT
+// 文字の場合 PDO::PARAM_STR
+
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute(); //実行
+
+//４．データ登録処理後
+//４．データ登録処理後
+if ($status == false) {
+    sql_error($stmt);
+} else {
+    redirect('select.php');
+}
+
